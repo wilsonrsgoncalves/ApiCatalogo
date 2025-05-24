f6152609bf3b94ff6ace8364698b2b101b894024
Wilson Gonçalves <wilssp@gmail.com>
2025-05-24 14:56:15 -0300

    Inicio do APICatalogo

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext( options )
+{
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,98 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutosController(AppDbContext context) : ControllerBase
+{
+    private readonly AppDbContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Produto>>> Get()
+    {
+        if (_context.Produtos == null)
+        {
+            return NotFound();
+        }
+
+        var produtos = await _context.Produtos.ToListAsync();
+        return produtos;
+    }
+
+    [HttpGet("{id:int:min(1)}", Name = "ObterProduto")]
+    public async Task<ActionResult<Produto>> Get(int id)
+    {
+        if (_context.Produtos == null)
+        {
+            return NotFound("Produtos not found...");
+        }
+
+        var produto = await _context.Produtos.FirstOrDefaultAsync(p => p.ProdutoId == id);
+        if (produto is null)
+        {
+            return NotFound("Produto não encontrado...");
+        }
+        return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        if (_context.Produtos == null)
+        {
+            return Problem("Entity set 'AppDbContext.Produtos' is null.");
+        }
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto",
+            new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.ProdutoId)
+        {
+            return BadRequest();
+        }
+
+        if (_context.Produtos == null)
+        {
+            return Problem("Entity set 'AppDbContext.Produtos' is null.");
+        }
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        if (_context.Produtos == null)
+        {
+            return NotFound("Produtos not found...");
+        }
+
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+        if (produto is null)
+        {
+            return NotFound("Produto não localizado...");
+        }
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+}
